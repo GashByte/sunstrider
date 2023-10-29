0272d67cc2f0acd4abca8b6ce7d117688ce63574
Zone Su Yuni <120739043+GashByte@users.noreply.github.com>
2023-10-29 17:45:48 +0800

    留着以后用

---
+++ src/srcipts/index/pointerSetter.js
@@ -0,0 +1,27 @@
+const cursor = document.querySelector('.pointer');
+const body = document.body;
+
+if(cursor){
+    window.onmousemove = (e) => {
+        cursor.style.transition = '0s';
+        cursor.style.transform = `translate(${e.clientX - 5}px, ${e.clientY}px)`;
+    }
+    
+    body.onmouseleave = (e) => {
+        cursor.style.transition = '.5s';
+        cursor.style.opacity = '0';
+    }
+    
+    body.onmouseenter = (e) => {
+        cursor.style.transition = '0s';
+        let opacity = 0;
+        const perStepAdd = 0.01;
+        const interval = setInterval(() => {
+            opacity += perStepAdd;
+            cursor.style.opacity = opacity;
+            if(cursor.style.opacity >= 1){
+                clearInterval(interval);
+            }
+        }, 5);
+    }
+}
